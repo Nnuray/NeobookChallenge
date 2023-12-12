230936f9d2564afc78d83fefc753670d96153e81
Nuray <nuray.kavkaeva.04@gmail.com>
2023-12-12 23:36:58 +0600

    first commit

---
+++ src/main/java/com/example/neobookchallenge/NeobookChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.neobookchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeobookChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeobookChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/neobookchallenge/NeobookChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neobookchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeobookChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
