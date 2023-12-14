9d120ffc589bedffe72e960ee23d97f4f296c130
Nuray <nuray.kavkaeva.04@gmail.com>
2023-12-15 00:23:05 +0600

    get

---
+++ src/main/java/com/example/neobookchallenge/controller/UserController.java
@@ -0,0 +1,5 @@
+package com.example.neobookchallenge.controller;
+
+public class UserController {
+
+}
--- src/main/java/com/example/neobookchallenge/entity/Product.java
+++ src/main/java/com/example/neobookchallenge/entity/Product.java
@@ -1,7 +1,9 @@
 package com.example.neobookchallenge.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
@@ -9,6 +11,8 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "products")
 public class Product {
     @Id
